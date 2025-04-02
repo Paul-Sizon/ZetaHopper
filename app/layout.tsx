34871eddcea18ee1da8625820e2aba2567b29ce0
paul.sizon <paul.sizon@outlook.com>
2025-04-01 22:14:17 -0300

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,28 @@ import Providers from './providers';
 
 export const metadata: Metadata = {
   title: 'ZetaHopper',
-  description: 'Onchain trading bot for Zetachain',  
+  description: 'Onchain trading bot for Zetachain',
+  openGraph: {
+    title: 'ZetaHopper',
+    description: 'Onchain trading bot for Zetachain',
+    url: 'https://zeta-hopper.vercel.app/',
+    siteName: 'ZetaHopper',
+    images: [
+      {
+        url: 'https://zeta-hopper.vercel.app/thumbnail_zetahopper.webp',
+        width: 1024,
+        height: 533,
+        alt: 'ZetaHopper Preview',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ZetaHopper',
+    description: 'Onchain trading bot for Zetachain',
+    images: ['https://zeta-hopper.vercel.app/thumbnail_zetahopper.webp'],
+  },
 }
 
 export default function RootLayout({
